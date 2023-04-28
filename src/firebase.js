bd8e793fdb83cd9841aa672efa154681af379246
John Jones <johnkeartonjones@googlemail.com>
2023-04-28 21:24:38 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
   var firebaseConfig = {
-    apiKey: "AIzaSyBGk7jYrWzrDBLfhqwXnYczqCjHjoY_g0k",
-    authDomain: "fitnesstracker-6afd6.firebaseapp.com",
-    databaseURL: "https://fitnesstracker-6afd6.firebaseio.com",
-    projectId: "fitnesstracker-6afd6",
-    storageBucket: "fitnesstracker-6afd6.appspot.com",
-    messagingSenderId: "265517822323",
-    appId: "1:265517822323:web:ddbba9f32b4db6598d5506"
+    
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig)
